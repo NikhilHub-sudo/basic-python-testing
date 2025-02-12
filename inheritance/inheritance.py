a3084166aa35409dd05611785dd52c6820fe9e61
“Nikhil” <nikhil.charmore@gmail.comgit config --global user.email nikhil.charmore@gmail.com>
2025-02-12 12:57:34 +0530

    inheritance folder added

---
+++ inheritance/inheritance.py
@@ -0,0 +1,54 @@
+# class A:
+#     def m(self):
+#         print("this is parent ")
+# class B(A):
+#     def m2(self):
+#         print("this is s child")
+#
+#
+# obj=B()
+# obj.m()
+# obj.m2()
+
+# class A:
+#     def m(self):
+#         print("parent")
+# class B(A):
+#     def m1(self):
+#         print("child")
+#         super().m()
+# obj=B()
+# obj.m1()
+
+# class Father:
+#     def father_func(self):
+#         print("father")
+# class Mother:
+#     def mother_func(self):
+#         print("mother")
+# class Child(Father,Mother):
+#     def child_func(self):
+#         print("child")
+# obj=Child()
+# obj.child_func()
+# obj.mother_func()
+# obj.father_func()
+
+# class Father:
+#     def father_func(self):
+#         print("father")
+# class Mother:
+#     def mother_func(self):
+#         print("mother")
+# class Child(Father,Mother):
+#     def child_func(self):
+#         print("child")
+#         super().father_func()
+#         super().mother_func()
+# obj=Child()
+# obj.child_func()
+
+
+
+
+
